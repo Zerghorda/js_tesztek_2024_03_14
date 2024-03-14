b925f9a2d4760d585220af5afd1821ff4a04db29
Tar Gergő <tar.gergo21@gmail.com>
2024-03-14 15:21:53 +0100

    assertek

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 import { tesztFugvenyek } from "./teszt.js";
 export function fuggvenyem(a, b) {
+  if (a === 0 || b === 0) {
+    return "Nem lehet nulla!";
+  }
+  if (a < 0 || b < 0) {
+    return "Minkettő számnak pozitivnak kell lennie!"
+  }
   while (a != b) {
     if (a > b) {
       a = a - b;
--- teszt.js
+++ teszt.js
@@ -5,8 +5,11 @@ export function tesztFugvenyek() {
     let a = 34;
     let b = 50;
     let vart = 2;
-    console.log(
-      `a:${a}, b:${b}, várt:${vart}, eredmeny:${vart === fuggvenyem(a, b)}, függvény eredmény:${fuggvenyem(a,b)}`
+    console.assert(
+      vart === fuggvenyem(a, b),
+      "%o",
+      `a=${a}, b=${b} ,eredmeny=${fuggvenyem(a, b)} várt=${vart}`,
+      "A várt eredmény nem stimmel"
     );
   }
   {
@@ -14,33 +17,90 @@ export function tesztFugvenyek() {
     let a = 15;
     let b = 50;
     let vart = 5;
-    console.log(
-      `a:${a}, b:${b}, várt:${vart}, eredmeny:${vart === fuggvenyem(a, b)}, függvény eredmény:${fuggvenyem(a,b)}`
+    console.assert(
+      vart === fuggvenyem(a, b),
+      "%o",
+      `a=${a}, b=${b} ,eredmeny=${fuggvenyem(a, b)} várt=${vart}`,
+      "A várt eredmény nem stimmel"
     );
   }
- /* {
+  {
     console.log("Teszt3");
-    let a = 34.9;
+    let a = -15;
     let b = 50;
-    let vart = "hiba";
-    console.log(
-      `a:${a}, b:${b}, várt:${vart}, eredmeny:${vart === fuggvenyem(a, b)}`
+    let vart = "Minkettő számnak pozitivnak kell lennie!";
+    console.assert(
+      vart === fuggvenyem(a, b),
+      "%o",
+      `a=${a}, b=${b} ,eredmeny=${fuggvenyem(a, b)} várt=${vart}`,
+      "A várt eredmény nem stimmel"
     );
-  }*/
- /* {
+  }
+  {
     console.log("Teszt4");
     let a = 0;
     let b = 5;
-    let vart = "hiba";
-    console.log(
-      `a:${a}, b:${b}, várt:${vart}, eredmeny:${vart === fuggvenyem(a, b)}`
+    let vart = "Nem lehet nulla!";
+    console.assert(
+      vart === fuggvenyem(a, b),
+      "%o",
+      `a=${a}, b=${b} ,eredmeny=${fuggvenyem(a, b)} várt=${vart}`,
+      "A várt eredmény nem stimmel"
     );
-  }*/
- /* {
-    console.log("Teszt4");
+  }
+  {
+    console.log("Teszt5");
     let a = 0;
     let b = 0;
-    let vart = 0;
+    let vart = "Nem lehet nulla!";
+    console.assert(
+      vart === fuggvenyem(a, b),
+      "%o",
+      `a=${a}, b=${b} ,eredmeny=${fuggvenyem(a, b)} várt=${vart}`,
+      "A várt eredmény nem stimmel"
+    );
+  }
+  {
+    console.log("Teszt6");
+    let a = 5;
+    let b = 5;
+    let vart = 5;
+    console.assert(
+      vart === fuggvenyem(a, b),
+      "%o",
+      `a=${a}, b=${b} ,eredmeny=${fuggvenyem(a, b)} várt=${vart}`,
+      "A várt eredmény nem stimmel"
+    );
+  }
+  {
+    console.log("Teszt7");
+    let a = 3;
+    let b = 4;
+    let vart = 1;
+    console.assert(
+      vart === fuggvenyem(a, b),
+      "%o",
+      `a=${a}, b=${b} ,eredmeny=${fuggvenyem(a, b)} várt=${vart}`,
+      "A várt eredmény nem stimmel"
+    );
+  }
+  {
+    console.log("Teszt8");
+    let a = 24;
+    let b = 16;
+    let vart = 8;
+    console.assert(
+      vart === fuggvenyem(a, b),
+      "%o",
+      `a=${a}, b=${b} ,eredmeny=${fuggvenyem(a, b)} várt=${vart}`,
+      "A várt eredmény nem stimmel"
+    );
+  }
+  /* {
+    console.log("Teszt4");
+    let a = Number.MAX_SAFE_INTEGER;
+    let b = 6;
+    let vart =5;
     console.log(
       `a:${a}, b:${b}, várt:${vart}, eredmeny:${vart === fuggvenyem(a, b)}`
     );
